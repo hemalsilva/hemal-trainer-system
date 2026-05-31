bf799526353a498810518609a4c6723a3ee6572c
Hemal Silva <hemal.silva@yahoo.com>
2026-05-31 23:40:37 +0530

    Wrap App in ErrorBoundary

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import axios from 'axios'
+
+if (window.location.protocol === 'file:') {
+  axios.defaults.baseURL = 'http://localhost:5000';
+}
+import App from './App.jsx';
+import ErrorBoundary from './ErrorBoundary.jsx';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary><App /></ErrorBoundary>
   </React.StrictMode>,
 )
