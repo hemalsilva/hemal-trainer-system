8bcbab35ebb5b02442293bcaf5fbe5fb845bcad6
Hemal Silva <hemal.silva@yahoo.com>
2026-06-01 23:47:35 +0530

    Add error catching to api/index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,1 @@
-const app = require(\'../backend/server.js\');\nmodule.exports = app;
+module.exports = (req, res) => { try { const app = require(`../backend/server.js`); return app(req, res); } catch (error) { res.status(500).json({ error: error.message, stack: error.stack }); } };
