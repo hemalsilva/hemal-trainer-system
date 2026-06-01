a8b30d3be137ae75c5affa0e8a79969ba0b768d0
Hemal Silva <hemal.silva@yahoo.com>
2026-06-01 23:27:25 +0530

    Fix server.js for Vercel

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,9 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+  });
+}
+module.exports = app;
