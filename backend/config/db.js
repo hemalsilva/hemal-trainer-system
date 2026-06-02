1c5d47e8c2c4dfa54e9c37194195bb29a6ee4847
Hemal Silva <hemal.silva@yahoo.com>
2026-06-02 19:57:30 +0530

    Patch db.js to accept POSTGRES_URL or DATABASE_URL

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,20 @@
 const { Pool } = require('pg');
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+
+let poolConfig = {
   ssl: { rejectUnauthorized: false }
-});
+};
+
+if (process.env.POSTGRES_URL) {
+  poolConfig.connectionString = process.env.POSTGRES_URL;
+} else if (process.env.DATABASE_URL) {
+  poolConfig.connectionString = process.env.DATABASE_URL;
+} else {
+  poolConfig.user = process.env.DB_USER;
+  poolConfig.host = process.env.DB_HOST;
+  poolConfig.database = process.env.DB_NAME;
+  poolConfig.password = process.env.DB_PASSWORD;
+  poolConfig.port = process.env.DB_PORT;
+}
+
+const pool = new Pool(poolConfig);
 module.exports = pool;
