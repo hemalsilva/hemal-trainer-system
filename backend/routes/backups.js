f02667bbf8582bf8a0f84a2feda7afe1ee1c8f82
Hemal Silva <hemal.silva@yahoo.com>
2026-06-29 23:09:28 +0530

    Fix archiver ESM require issue crashing Vercel

--- backend/routes/backups.js
+++ backend/routes/backups.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
-const archiver = require('archiver');
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
